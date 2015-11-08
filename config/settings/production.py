a37f86f58fe68091bc26553024e703dfab1806dd
Guillermo Alvarez <guialante@gmail.com>
2015-11-08 11:43:11 -0500

    added postgis engine to production

--- config/settings/production.py
+++ config/settings/production.py
@@ -119,7 +119,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 # ------------------------------------------------------------------------------
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
 DATABASES['default'] = env.db("DATABASE_URL")
-
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 # CACHING
 # ------------------------------------------------------------------------------
 # Heroku URL does not pass the DB number, so we parse it in
