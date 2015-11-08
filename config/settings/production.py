bf2304ac584116fc1c7a167bce761580389e4aa7
Guillermo Alvarez <guialante@gmail.com>
2015-11-08 15:30:18 -0500

    setting allow host

--- config/settings/production.py
+++ config/settings/production.py
@@ -54,7 +54,7 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['ec2-52-33-128-23.us-west-2.compute.amazonaws.com'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ("gunicorn", )
